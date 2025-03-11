6478454c7482d5580a6e8afe512f453b5be7fd2b
Mathias <mathiasbergqvist88@gmail.com>
2025-03-11 10:01:32 +0100

    Use button component i Schema

--- src/components/schema/Schema.tsx
+++ src/components/schema/Schema.tsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import styles from "./Schema.module.css";
 import { Talk } from "../../models/Talk";
 import { ScheduleDay } from "../../models/ScheduleDay";
+import Button from "../../design-system/Button/Button";
+import {
+  ButtonIcon,
+  ButtonType,
+} from "../../design-system/Button/Button.enums";
 
 const Schema = () => {
   const [schema, setSchema] = useState<Array<Talk>>([]);
@@ -45,30 +50,30 @@ const Schema = () => {
     <>
       <div className={styles.buttonGroup}>
         Välj dag:
-        <button
+        <Button
+          text="Alla dagar"
+          buttonType={ButtonType.secondary}
+          icon={ButtonIcon.hand}
+          iconLabel="Emoji med öppna händer"
           onClick={() => handleFilter(ScheduleDay.unset)}
-          className={`${styles.filterButton} ${
-            dayFilter === ScheduleDay.unset && styles.filterButtonSelected
-          }`}
-        >
-          Alla dagar
-        </button>
-        <button
+          selected={dayFilter === ScheduleDay.unset}
+        />
+        <Button
+          text="2023-03-21"
+          buttonType={ButtonType.secondary}
+          icon={ButtonIcon.one_finger}
+          iconLabel="Emoji med en hand som sträcker upp ett finger"
           onClick={() => handleFilter(ScheduleDay.friday)}
-          className={`${styles.filterButton} ${
-            dayFilter === ScheduleDay.friday && styles.filterButtonSelected
-          }`}
-        >
-          2023-03-21
-        </button>
-        <button
+          selected={dayFilter === ScheduleDay.friday}
+        />
+        <Button
+          text="2023-03-22"
+          buttonType={ButtonType.secondary}
+          icon={ButtonIcon.two_fingers}
+          iconLabel="Emoji med en hand som sträcker upp två fingrar"
           onClick={() => handleFilter(ScheduleDay.saturday)}
-          className={`${styles.filterButton} ${
-            dayFilter === ScheduleDay.saturday && styles.filterButtonSelected
-          }`}
-        >
-          2023-03-22
-        </button>
+          selected={dayFilter === ScheduleDay.saturday}
+        />
       </div>
       {filteredSchema.map((talk) => {
         const { title, speaker, date, description } = talk;
@@ -81,7 +86,12 @@ const Schema = () => {
             <p className={styles.infoCardDescription}>{description}</p>
             <div className={styles.infoCardFooter}>
               <span className={styles.infoCardAuthor}>Av {speaker}</span>
-              <button className={styles.infoCardButton}>Läs Mer</button>
+              <Button
+                text="Läs Mer"
+                buttonType={ButtonType.primary}
+                icon={ButtonIcon.arrow}
+                iconLabel="Emoji med en pil"
+              />
             </div>
           </div>
         );
